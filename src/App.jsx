a3a1cc0c9e74939324678c78056e1524b1e86c71
Quranabev Muhammad <mxmmad17@gmail.com>
2024-05-04 23:24:56 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { Header, Footer, Modal } from "./components"
-import HomePage from "./pages/home"
 import { Route, Routes } from "react-router-dom"
 import { routes } from "./utils/routes"
 
@@ -18,7 +17,6 @@ function App() {
             ))
           }
         </Routes>
-        <HomePage />
       </main>
       <Footer />
       <Modal setModalOpen={setModalOpen} modalOpen={modalOpen} />
